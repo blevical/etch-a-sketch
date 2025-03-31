cc676b8c9756e9ee845c540966029486aeb1448e
Caleb Blevins <blevsca2@gmail.com>
2025-03-31 17:00:02 -0400

    Grid created, button operational.

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const newDiv = document.getElementById("container");
 const button = document.querySelector("button");
-const rows = document.getElementsByClassName("row")
 
-function makeSquares(){
+
+function createSquares() {
     for (let i = 0; i < 16 * 16; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
-        row.appendChild(square);    
+        newDiv.appendChild(square); 
     }
 }
-function makeGrid () {
-    for (let j = 0; j < 16; j++) {
-        let column = document.createElement("div")
-        let row = document.createElement("div")
-        column.classList.add("column")
-        row.classList.add("row")
-        column.appendChild(row)
-        newDiv.appendChild(column)
-    }
+createSquares();
+button.addEventListener("click", createSquares);
+
+function sketch() {
+    addEventListener("mouseover", square)
+    
 }
-makeGrid(makeSquares)
