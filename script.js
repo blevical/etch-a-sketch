723d4cd7d658a9f1319fe04fc8ecbeef4465e1ea
Caleb Blevins <blevsca2@gmail.com>
2025-04-02 13:04:12 -0400

    etch-a-sketch is almost operational. I am running into the problem of having it sketch on the new squares that are being created.

--- script.js
+++ script.js
@@ -6,13 +6,32 @@ function createSquares() {
     for (let i = 0; i < 16 * 16; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
-        newDiv.appendChild(square); 
+        newDiv.appendChild(square);
     }
 }
 createSquares();
 button.addEventListener("click", createSquares);
+clear.addEventListener("click", clearSquares);
 
 function sketch() {
-    addEventListener("mouseover", square)
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseover",() => {
+            square.style.backgroundColor = "blue";
+            
+        square.addEventListener("mouseleave", () => {
+            square.style.backgroundColor = "black";
+        }
+        )
+        });
+    });
     
+}
+sketch(createSquares(button.addEventListener("click", createSquares)));
+
+function clearSquares () {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.style.backgroundColor = "";
+    })
 }
